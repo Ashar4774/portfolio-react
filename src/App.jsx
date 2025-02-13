572eaf46589acbe3574b1511492c4248bd93252c
Ashar4774 <ashar.muhammad74@gmail.com>
2025-02-13 21:30:08 +0500

    initially uploaded product on github

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {React, useEffect} from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Hero from './components/Hero.jsx'
+
+function App() {
+    useEffect(()=>{
+        AOS.init({
+         duration: 1000,
+        })
+    }, [])
+
+  return (
+      <main className='bg-gray-950'>
+        <Hero />
+      </main>
+  )
+}
+
+export default App
